c4ca647bffc9a9b06a378beb80d488a3ef591cbb
nghhuyhoang <jewang.eulo@gmail.com>
2024-01-12 20:14:41 +0100

    clear-lib

--- maps/function.py
+++ maps/function.py
@@ -10,16 +10,3 @@ def dist(lat1, long1, lat2, long2):
     c = 2 * np.arctan2(np.sqrt(a), np.sqrt(1-a))
     return float("{:.2f}".format(R * c))
 
-from DeepImageSearch import Load_Data, Search_Setup
-def findSimilar():
-    # Load images from a folder
-    image_list = Load_Data().from_folder(['images'])
-    # Set up the search engine, You can load 'vit_base_patch16_224_in21k', 'resnet50' etc more then 500+ models 
-    st = Search_Setup(image_list=image_list, model_name='vgg19', pretrained=True, image_count=100)
-    # Index the images
-    st.run_index()
-    # Get metadata
-    metadata = st.get_image_metadata_file()
-    # Plot similar images
-    st.plot_similar_images(image_path='images/Experimental Cocktail Club/Image_4.jpg', number_of_images=1)
-    return None
--- maps/views.py
+++ maps/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
 from .models import Location, Paris
-from geopy.geocoders import Nominatim
 from .function import dist, findSimilar
 import pandas as pd 
 import os
